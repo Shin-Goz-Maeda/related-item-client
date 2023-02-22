75ee43e13ff4e7f996be936da1001c1b7d20008d
Shin-Goz-Maeda <km.tks.mml710@gmail.com>
2023-02-22 16:15:06 +0900

    ディレクトリ構成を見直し

---
+++ src/jsx/views/components/blocks/mainItemContainer/itemInfoContainer/ItemInfoContainer.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const ItemInfoContainer = (props) => {
+  const { itemName, brand, itemCategory } = props;
+
+  return (
+    <ItemInfoContent id='itemInfo' className='itemInfo'>
+      <ItemInfoLists>
+        <ItemInfoList id='brand' className='item'>{brand}</ItemInfoList>
+        <ItemInfoList id='itemName' className='item'>{itemName}</ItemInfoList>
+        <ItemInfoList id='itemCategory' className='item'>{itemCategory}</ItemInfoList>
+      </ItemInfoLists>
+    </ItemInfoContent>
+  );
+};
+
+const ItemInfoContent = styled.div`
+  width: 95%;
+  height: 30%;
+  border: 2px solid red;
+  margin-bottom: 5px;
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+const ItemInfoLists = styled.ul`
+  padding: 0;
+`;
+
+const ItemInfoList = styled.li`
+  list-style: none;
+`;
+
+export default ItemInfoContainer;
---
+++ src/jsx/views/components/blocks/menu/Menu.jsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import MenuData from '../menu/MenuData';
+
+const Menu = () => {
+  return (
+    <SidebarContainer>
+      <SidebarLists>
+        {MenuData.map((value, key) => {
+          return (
+            <SidebarList key={key}>
+              <Link to={value.link}>
+                <ListIcon>{value.icon}</ListIcon>
+                <ListTitle>{value.title}</ListTitle>
+              </Link>
+            </SidebarList>
+          );
+        })};
+      </SidebarLists>
+    </SidebarContainer>
+  );
+};
+
+const SidebarContainer = styled.div`
+  height: 100%;
+  width: 20%;
+  min-width: 300px;
+  background-color: #3b627a;
+`;
+
+const SidebarLists = styled.ul`
+  height: auto;
+  width: 100%
+  list-style: none;
+`;
+
+const SidebarList = styled.li`
+  width: 100%;
+  &:hover {
+    cursor: pointer;
+    background-color: lightgreen;
+  }
+`;
+
+const ListIcon = styled.div`
+  height: 60px;
+  color: white;
+`;
+
+const ListTitle = styled.div`
+  height: 60px;
+  color: white;
+`;
+
+export default Menu;
